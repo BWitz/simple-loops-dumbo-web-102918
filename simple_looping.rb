e38d2ef5fc963dcfe4f9440b2ef2cd317e19ee77
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-10 01:31:22 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -51,6 +51,7 @@ def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times = 1..7
   for number_of_times in 1..7
-  
+  puts phrase
+end
 end
 
